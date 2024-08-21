b10e1ff32dea94604663402b0f5a6e576f15d2c4
Jay200123 <kenken.ayonon@gmail.com>
2024-08-21 21:07:49 +0800

    Add middleware error

--- config/express-config.js
+++ config/express-config.js
@@ -1,8 +1,11 @@
+const { errorJson, errorHandler } = require("../middleware/errorJson");
 const express = require("express");
 const router = express.Router();
 const app = express();
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
+app.use(errorJson);
+app.use(errorHandler);
 
 module.exports = { router, app };
