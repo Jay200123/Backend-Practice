2347959dc25af9cdcb03cfcf09d0f05b46a8f719
Jay200123 <kenken.ayonon@gmail.com>
2024-08-21 21:08:09 +0800

    Export Error constant

--- constants/index.js
+++ constants/index.js
@@ -2,10 +2,12 @@ const RESOURCE = require("./resource");
 const METHOD = require("./method");
 const PATH = require("./path");
 const STATUSCODE = require("./statusCode");
+const ERROR = require("./error");
 
 module.exports = {
     RESOURCE,
     METHOD,
     PATH,
     STATUSCODE,
+    ERROR
 }
