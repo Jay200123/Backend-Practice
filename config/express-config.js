06eae72548ec574487c84ae02eae8fd01698665e
Jay200123 <kenken.ayonon@gmail.com>
2024-08-26 22:12:24 +0800

    Refactor express-config file

--- config/express-config.js
+++ config/express-config.js
@@ -1,11 +1,5 @@
-const { errorJson, errorHandler } = require("../middleware/errorJson");
 const express = require("express");
 const router = express.Router();
 const app = express();
 
-router.use(express.json());
-router.use(express.urlencoded({ extended: true }));
-app.use(errorJson);
-app.use(errorHandler);
-
-module.exports = { router, app };
+module.exports = { express, router, app };
