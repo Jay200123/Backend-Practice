e5a86294cbeabcb4a3642b40e4e84b31c435e3c2
Jay200123 <kenken.ayonon@gmail.com>
2024-08-26 22:12:39 +0800

    Refactor mongo db config file

--- config/config.js
+++ config/config.js
@@ -1,16 +1,15 @@
 const mongoose = require("mongoose");
-const { RESOURCE } = require("../constants/index");
+const { RESOURCE, STATUSCODE } = require("../constants/index");
+const logger = require("../utils/logger");
 
 const connectDB = async () => {
   try {
     mongoose.set(RESOURCE.STRICT, false);
     await mongoose.connect(process.env.DATABASE_URI);
   } catch (err) {
-    console.log(err);
+    logger.info(err);
+    process.exit(STATUSCODE.ONE);
   }
-  console.log(
-    `Mongoose database cloud connection successfully established with host ${mongoose.connection.host}`
-  );
 };
 
 module.exports = connectDB;
