80ebf3b034f8cf0ba91184864eaffc7253c04aff
Jay200123 <kenken.ayonon@gmail.com>
2024-08-27 08:47:24 +0800

    Add logger

--- middleware/errorJson.js
+++ middleware/errorJson.js
@@ -1,5 +1,6 @@
 const ErrorHandler = require("../utils/errorHandler");
 const { STATUSCODE, ERROR } = require("../constants/index");
+const logger = require("../utils/logger");  
 
 const errorJson = (err, req, res, next) => {
   if (!(err instanceof ErrorHandler)) {
@@ -11,6 +12,8 @@ const errorJson = (err, req, res, next) => {
 const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || STATUSCODE.SERVER_ERROR;
   const message = err.message || ERROR.INTERNAL_SERVER_ERROR;
+
+  logger.info(`Error: ${message}`);
 
   res.status(statusCode).json({
     success: false,
