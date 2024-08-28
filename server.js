cb803725e845cbc71919b598dcbd8f217bdce61a
Jay200123 <kenken.ayonon@gmail.com>
2024-08-28 10:05:30 +0800

    ADD path constants

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const { app, express } = require("./config/express-config.js");
 const { errorJson, errorHandler } = require("./middleware/errorJson");
 const logger = require("./utils/logger.js");
 const mongoose = require("mongoose");
-const { RESOURCE, STATUSCODE } = require("./constants/index.js");
+const { RESOURCE, STATUSCODE, PATH } = require("./constants/index.js");
 
 const globalEnvironment = require("./config/env-config.js");
 globalEnvironment();
@@ -15,12 +15,13 @@ app.use(express.urlencoded({ extended: true }));
 const routes = require("./routes/index.js");
 
 app.use(`${RESOURCE.API}${RESOURCE.V1}`, routes);
-app.get("/", async (req, res, next) => {
+
+app.get(PATH.INDEX, async (req, res, next) => {
   const data = { message: "Express Server Running Successfully..." };
   res.status(200).json(data);
 });
 
-app.all("*", (req, res, next) => {
+app.all(PATH.NOT_FOUND, (req, res, next) => {
   const error = new Error(`Route not found - ${req.originalUrl}`);
   error.status = STATUSCODE.NOT_FOUND;
   next(error);
