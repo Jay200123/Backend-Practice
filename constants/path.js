140a56dc4966e8b970daf38584de7e2955d31770
Jay200123 <kenken.ayonon@gmail.com>
2024-08-28 10:05:48 +0800

    Add index & not found path constants

--- constants/path.js
+++ constants/path.js
@@ -1,4 +1,8 @@
+const { NOT_FOUND } = require("./statusCode");
+
 module.exports = {
+  INDEX: "/",
+  NOT_FOUND: "*",
   USERS: "/users",
   USER_ID: "/user/:id",
   EDIT_USER_ID: "/user/edit/:id",
