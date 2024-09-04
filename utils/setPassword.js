4e09565a02722e6a01340e9d67c329817231edd6
Jay200123 <kenken.ayonon@gmail.com>
2024-09-04 17:40:17 +0800

    Add Number Conversion to SALT ENV

--- utils/setPassword.js
+++ utils/setPassword.js
@@ -1,7 +1,7 @@
 const bcrypt = require("bcrypt");
 
 const setPassword = async(password)=>{
-    const hashPassword = await bcrypt.hash(password, process.env.SALT_NUMBER);
+    const hashPassword = await bcrypt.hash(password, Number(process.env.SALT_NUMBER));
     return hashPassword;
 };
 
