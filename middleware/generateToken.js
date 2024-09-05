6acaa1cc1b37329baa09456ff584cf2a2dca7ed1
Jay200123 <kenken.ayonon@gmail.com>
2024-09-05 15:56:01 +0800

    Remove generateToken file

--- middleware/generateToken.js
+++
@@ -1,8 +0,0 @@
-const token = require("jsonwebtoken");
-const { RESOURCE } = require("../constants/index");
-
-const generateToken = (payload = {}, expiresIn = RESOURCE.ONE_DAY) => {
-    return token.sign(payload, process.env.ACCESS_TOKEN_SECRET, {expiresIn})
-};
-
-module.exports = generateToken;
