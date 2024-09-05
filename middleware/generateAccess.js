29ad2765dad2f5e747a811ff8cc5b9781742fdf6
Jay200123 <kenken.ayonon@gmail.com>
2024-09-05 15:55:50 +0800

    remove generateAccess file

--- middleware/generateAccess.js
+++
@@ -1,9 +0,0 @@
-const generateToken = require("./generateToken");
-const { RESOURCE } = require("../constants/index");
-
-const generateAccessToken = (payload = {}) => {
-  const accessToken = generateToken(payload, RESOURCE.ONE_DAY);
-  return { access: accessToken };
-};
-
-module.exports = generateAccessToken;
