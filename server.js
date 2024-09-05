5bc91c9fb4a90628e54175b9e69f269857a74d64
Jay200123 <kenken.ayonon@gmail.com>
2024-09-05 10:59:44 +0800

    Refactor server.js

--- server.js
+++ server.js
@@ -1,34 +1,14 @@
-const { app, express } = require("./config/express-config.js");
-const { errorJson, errorHandler } = require("./middleware/errorJson");
 const logger = require("./utils/logger.js");
 const mongoose = require("mongoose");
-const { RESOURCE, STATUSCODE, PATH } = require("./constants/index.js");
+const { RESOURCE, STATUSCODE } = require("./constants/index.js");
+
+const app = require("./app/index.js");
 
 const globalEnvironment = require("./config/env-config.js");
 globalEnvironment();
+
 const connectDB = require("./config/db-config.js");
 connectDB();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-const routes = require("./routes/index.js");
-
-app.use(`${RESOURCE.API}${RESOURCE.V1}`, routes);
-
-app.get(PATH.INDEX, async (req, res, next) => {
-  const data = { message: "Express Server Running Successfully..." };
-  res.status(200).json(data);
-});
-
-app.all(PATH.NOT_FOUND, (req, res, next) => {
-  const error = new Error(`Route not found - ${req.originalUrl}`);
-  error.status = STATUSCODE.NOT_FOUND;
-  next(error);
-});
-
-app.use(errorJson);
-app.use(errorHandler);
 
 mongoose.connection.once(RESOURCE.OPEN, () => {
   app.listen(process.env.PORT);
