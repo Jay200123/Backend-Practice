719dd82bebaba674f84cc8818ea6a3b4475bb609
Jay200123 <kenken.ayonon@gmail.com>
2024-09-05 15:55:21 +0800

    Refactor app index.js

--- app/index.js
+++ app/index.js
@@ -1,19 +1,20 @@
 const { app, express } = require("../config/express-config.js");
 const { errorJson, errorHandler } = require("../middleware/errorJson");
 const { RESOURCE, STATUSCODE, PATH } = require("../constants/index.js");
-
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const routes = require("../routes/index.js");
-
-app.use(`${RESOURCE.API}${RESOURCE.V1}`, routes);
 
 app.get(PATH.INDEX, async (req, res, next) => {
   const data = { message: "Express Server Running Successfully..." };
   res.status(200).json(data);
 });
+
+app.use(`${RESOURCE.API}${RESOURCE.V1}`, routes);
 
 app.all(PATH.NOT_FOUND, (req, res, next) => {
   const error = new Error(`Route not found - ${req.originalUrl}`);
