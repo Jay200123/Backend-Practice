0aaa6e58d65ece89f3ee928797d6d0de0c8b907c
Jay200123 <kenken.ayonon@gmail.com>
2024-09-06 16:43:18 +0800

    Add role

--- constants/index.js
+++ constants/index.js
@@ -3,11 +3,13 @@ const METHOD = require("./method");
 const PATH = require("./path");
 const STATUSCODE = require("./statusCode");
 const ERROR = require("./error");
+const ROLE = require("./role");
 
 module.exports = {
-    RESOURCE,
-    METHOD,
-    PATH,
-    STATUSCODE,
-    ERROR
-}
+  RESOURCE,
+  METHOD,
+  PATH,
+  STATUSCODE,
+  ERROR,
+  ROLE,
+};
