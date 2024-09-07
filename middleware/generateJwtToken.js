08552830b35033cb90d66b7da059390f670e304a
Jay200123 <kenken.ayonon@gmail.com>
2024-09-07 21:40:05 +0800

    Remove commented codes

--- middleware/generateJwtToken.js
+++ middleware/generateJwtToken.js
@@ -8,12 +8,3 @@ const generateJwtToken = function(Id){
   };
   
   module.exports = generateJwtToken;
-
-// const token = require("jsonwebtoken");
-// const { RESOURCE } = require("../constants/index");
-
-// const generateToken = (payload = {}, expiresIn = RESOURCE.ONE_DAY) => {
-//     return token.sign(payload, process.env.ACCESS_TOKEN_SECRET, {expiresIn})
-// };
-
-// module.exports = generateToken;
