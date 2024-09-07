c1008468a6e15a509b08070cee96fe696fac8e0c
Jay200123 <kenken.ayonon@gmail.com>
2024-09-07 21:39:54 +0800

    Fix cookie expire time to 1 day

--- middleware/sendToken.js
+++ middleware/sendToken.js
@@ -6,7 +6,7 @@ const sendToken = (res, data, message = {}, statusCode) => {
 
   const options = {
     expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 24 * 60 * 60 * 1000 // 1 week expiration*
+      Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000,
     ),
     httpOnly: true,
   };
