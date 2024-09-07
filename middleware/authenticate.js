272f09f8d74853abb577793f1bcffce67a1f5498
Jay200123 <kenken.ayonon@gmail.com>
2024-09-07 22:15:15 +0800

    Refactor authenticate.js

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,24 +1,24 @@
-const service = require("../service/userService.js");
-const jwt = require("jsonwebtoken");
-const { ErrorHandler } = require("../utils/index.js");
-const { STATUSCODE } = require("../constants/index");
+const service = require('../service/userService.js')
+const jwt = require('jsonwebtoken')
+const { ErrorHandler } = require('../utils/index.js')
+const { STATUSCODE } = require('../constants/index')
 
 const isAuthenticated = async (req, res, next) => {
-  const hasToken = req.cookies.token;
-
-  if (!hasToken) {
-    return next(new ErrorHandler("Login First", STATUSCODE.UNAUTHORIZED));
+  if (!req.cookies.token) {
+    return next(new ErrorHandler('Login First', STATUSCODE.UNAUTHORIZED))
   }
 
-  const decode = jwt.verify(hasToken, process.env.ACCESS_TOKEN_SECRET);
-  const data = await service.getById(decode?.id);
-  req.user = data;
-  next();
-};
+  const decode = jwt.verify(req.cookies.token, process.env.ACCESS_TOKEN_SECRET)
+  const data = await service.getById(decode?.id)
+  req.user = data
+  console.log(req.user);
+  next()
+}
 
 const userRole = (...roles) => {
   return (req, res, next) => {
-    console.log(req.user);
+    // console.log(req.user);
+    next()
     // if (!roles.includes(req.user.role)) {
     //   return next(
     //     new ErrorHandler(
@@ -27,7 +27,8 @@ const userRole = (...roles) => {
     //     )
     //   );
     // }
-  };
-};
+    // next();
+  }
+}
 
-module.exports = { isAuthenticated, userRole };
+module.exports = { isAuthenticated, userRole }
