3e03a5cf826f6aafa7f5ce5a2f007a8eeb5bc80e
Jay200123 <kenken.ayonon@gmail.com>
2024-09-21 19:41:17 +0800

    Remove item model

--- model/item.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");   
-const { RESOURCE } = require("../constants/index.js");
-
-const itemSchema = new mongoose.Schema({
-    
-});
-
-module.exports = mongoose.model(RESOURCE.ITEM, itemSchema);    
