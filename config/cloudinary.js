87794b7bc7e6a0a609a8edd0f25f300a6ee20621
Jay200123 <kenken.ayonon@gmail.com>
2024-09-21 16:30:05 +0800

    Refactor cloudinary

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,11 @@
-const cloudinary = require("cloudinary").v2;
+const cloudinary = require('cloudinary').v2
+const globalEnviroment = require("./env-config");
+globalEnviroment();
 
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-module.exports = cloudinary;    
+module.exports = cloudinary
