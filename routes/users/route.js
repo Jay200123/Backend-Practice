dc906faf323e82dc4cfee9e09211aa253e250d94
Jay200123 <kenken.ayonon@gmail.com>
2024-09-21 19:42:50 +0800

    Add user route

---
+++ routes/users/route.js
@@ -0,0 +1,42 @@
+const { router } = require('../../config/index.js')
+const { METHOD, PATH, ROLE } = require('../../constants/index.js')
+const userController = require('./controller.js')
+const { isAuthenticated, userRole } = require('../../middleware/index.js')
+
+const userRoutes = [
+  {
+    method: METHOD.GET,
+    path: PATH.USERS,
+    middleware: [isAuthenticated],
+    role: [ROLE.ADMIN],
+    handler: userController.getAllUsers
+  },
+  {
+    method: METHOD.GET,
+    path: PATH.USER_ID,
+    middleware: [isAuthenticated],
+    role: [ROLE.ADMIN, ROLE.CUSTOMER],
+    handler: userController.getOneUser
+  },
+  {
+    method: METHOD.PATCH,
+    path: PATH.EDIT_USER_ID,
+    middleware: [isAuthenticated],
+    role: [ROLE.ADMIN, ROLE.CUSTOMER],
+    handler: userController.updateUser
+  },
+  {
+    method: METHOD.DELETE,
+    path: PATH.USER_ID,
+    middleware: [isAuthenticated],
+    role: [ROLE.ADMIN],
+    handler: userController.deleteUser
+  }
+]
+
+userRoutes.forEach(route => {
+  const { method, path, middleware = [], role = [], handler } = route
+  router[method](path, middleware.concat(userRole(...role)), handler)
+})
+
+module.exports = router
