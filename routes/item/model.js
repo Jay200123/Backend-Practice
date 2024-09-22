1586badb9e8c12bd215d7096f1a02070be8ff05b
Jay200123 <kenken.ayonon@gmail.com>
2024-09-22 20:46:54 +0800

    Add item model

---
+++ routes/item/model.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const { RESOURCE } = require('../../constants/index')
+
+const itemSchema = new mongoose.Schema({
+  item_name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  date_purchased: {
+    type: Date,
+    required: true
+  },
+  brand_name: {
+    type: String,
+    required: true
+  },
+  image: [
+    {
+      public_id: String,
+      url: String,
+      originalname: String
+    }
+  ],
+});
+
+module.exports = mongoose.model(RESOURCE.ITEM, itemSchema); 
