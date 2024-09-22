5c86c02a156e20307beb3f50df4dca283c1992fc
Jay200123 <kenken.ayonon@gmail.com>
2024-09-22 20:57:02 +0800

    Fix route api code block position

--- app/index.js
+++ app/index.js
@@ -14,13 +14,14 @@ app.get(PATH.INDEX, async (req, res, next) => {
   res.status(STATUSCODE.SUCCESS).json(data);
 });
 
-app.use(`${RESOURCE.API}${RESOURCE.V1}`, routes);
-
-app.all(PATH.NOT_FOUND, (req, res, next) => {
+app.all("*", (req, res, next) => {
   const error = new Error(`Route not found - ${req.originalUrl}`);
   error.status = STATUSCODE.NOT_FOUND;
   next(error);
 });
+
+app.use(`${RESOURCE.API}${RESOURCE.V1}`, routes);
+
 
 app.use(errorJson);
 app.use(errorHandler);
