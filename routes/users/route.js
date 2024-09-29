13f79fd15dec85062637b29b38610bf8fd09c3a9
Jay200123 <kenken.ayonon@gmail.com>
2024-09-29 21:48:18 +0800

    Add sendOTP route

--- routes/users/route.js
+++ routes/users/route.js
@@ -1,7 +1,8 @@
-const { router } = require('../../config/index.js')
-const { METHOD, PATH, ROLE } = require('../../constants/index.js')
-const userController = require('./controller.js')
-const { isAuthenticated, userRole } = require('../../middleware/index.js')
+const { router } = require("../../config/index.js");
+const { METHOD, PATH, ROLE } = require("../../constants/index.js");
+const userController = require("./controller.js");
+const { isAuthenticated, userRole } = require("../../middleware/index.js");
+const method = require("../../constants/method.js");
 
 const userRoutes = [
   {
@@ -9,34 +10,41 @@ const userRoutes = [
     path: PATH.USERS,
     middleware: [isAuthenticated],
     role: [ROLE.ADMIN],
-    handler: userController.getAllUsers
+    handler: userController.getAllUsers,
   },
   {
     method: METHOD.GET,
     path: PATH.USER_ID,
     middleware: [isAuthenticated],
     role: [ROLE.ADMIN, ROLE.CUSTOMER],
-    handler: userController.getOneUser
+    handler: userController.getOneUser,
   },
   {
     method: METHOD.PATCH,
     path: PATH.EDIT_USER_ID,
     middleware: [isAuthenticated],
     role: [ROLE.ADMIN, ROLE.CUSTOMER],
-    handler: userController.updateUser
+    handler: userController.updateUser,
   },
   {
     method: METHOD.DELETE,
     path: PATH.USER_ID,
     middleware: [isAuthenticated],
     role: [ROLE.ADMIN],
-    handler: userController.deleteUser
-  }
-]
+    handler: userController.deleteUser,
+  },
+  {
+    method: METHOD.POST,
+    path: PATH.USER_OTP,
+    middleware: [isAuthenticated],
+    role: [ROLE.ADMIN, ROLE.CUSTOMER],
+    handler: userController.sendEmailOTP,
+  },
+];
 
-userRoutes.forEach(route => {
-  const { method, path, middleware = [], role = [], handler } = route
-  router[method](path, middleware.concat(userRole(...role)), handler)
-})
+userRoutes.forEach((route) => {
+  const { method, path, middleware = [], role = [], handler } = route;
+  router[method](path, middleware.concat(userRole(...role)), handler);
+});
 
-module.exports = router
+module.exports = router;
