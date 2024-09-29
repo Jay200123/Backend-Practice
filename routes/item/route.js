d03d27c8d83b03f2cad0c62907860b947b1f9813
Jay200123 <kenken.ayonon@gmail.com>
2024-09-29 21:47:28 +0800

    Refactor item route

--- routes/item/route.js
+++ routes/item/route.js
@@ -1,38 +1,49 @@
-const { router } = require('../../config/index')
-const itemController = require('./controller')
-const { PATH, METHOD } = require('../../constants/index')
+const { router } = require("../../config/index");
+const itemController = require("./controller");
+const { PATH, METHOD, ROLE } = require("../../constants/index");
+const { isAuthenticated, userRole } = require("../../middleware/index");
 
 const itemRouter = [
   {
     method: METHOD.GET,
     path: PATH.ITEMS,
-    handler: itemController.getAllItems
+    middleware: [isAuthenticated],
+    role: [ROLE.ADMIN],
+    handler: itemController.getAllItems,
   },
   {
     method: METHOD.GET,
     path: PATH.ITEM_ID,
-    handler: itemController.getOneItem
+    middleware: [isAuthenticated],
+    role: [ROLE.ADMIN],
+    handler: itemController.getOneItem,
   },
   {
     method: METHOD.POST,
     path: PATH.ITEMS,
-    handler: itemController.createItem
+    middleware: [isAuthenticated],
+    role: [ROLE.ADMIN],
+    handler: itemController.createItem,
   },
   {
     method: METHOD.PATCH,
     path: PATH.EDIT_ITEM_ID,
-    handler: itemController.updateItem
+    middleware: [isAuthenticated],
+    role: [ROLE.ADMIN],
+    handler: itemController.updateItem,
   },
   {
     method: METHOD.DELETE,
     path: PATH.ITEM_ID,
-    handler: itemController.deleteOneItem
-  }
-]
+    middleware: [isAuthenticated],
+    role: [ROLE.ADMIN],
+    handler: itemController.deleteOneItem,
+  },
+];
 
-itemRouter.forEach(route => {
-  const { path, method, handler } = route
-  router[method](path, handler)
+itemRouter.forEach((route) => {
+  const { path, method, middleware = [], role = [], handler } = route;
+  router[method](path, middleware.concat(userRole(...role)), handler);
 });
 
 module.exports = router;
