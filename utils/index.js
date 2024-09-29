30db76364e5c37b9c6b3fb03048df97e6635bca1
Jay200123 <kenken.ayonon@gmail.com>
2024-09-29 21:49:35 +0800

    import & export email, generateRandomCode

--- utils/index.js
+++ utils/index.js
@@ -2,15 +2,18 @@ const ErrorHandler = require("./errorHandler");
 const SuccessHandler = require("./successHandler");
 const logger = require("./logger");
 const upload = require("./multer");
-const imageUpload  = require("./imageUpload");
+const imageUpload = require("./imageUpload");
 const setPassword = require("./setPassword");
-
+const sendEmail = require("./sendEmail");
+const generateRandomCode = require("./generateCode");
 
 module.exports = {
-    ErrorHandler,
-    SuccessHandler,
-    logger,
-    upload,
-    imageUpload,
-    setPassword,
-}
+  ErrorHandler,
+  SuccessHandler,
+  logger,
+  upload,
+  imageUpload,
+  setPassword,
+  sendEmail,
+  generateRandomCode,
+};
