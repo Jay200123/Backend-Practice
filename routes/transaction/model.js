f5d4cb5b10540db086a7edce15bb628fda64101e
Jay200123 <kenken.ayonon@gmail.com>
2024-10-05 21:12:22 +0800

    Add new properties to transaction model

---
+++ routes/transaction/model.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const { RESOURCE } = require('../../constants/index')
+
+const transactionSchema = new mongoose.Schema({
+  date: {
+    required: true,
+    default: Date.now
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: RESOURCE.USER,
+    required: true
+  },
+  item: [
+    {
+      item: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: RESOURCE.ITEM,
+        required: true
+      },
+      quantity: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+
+  accessory: [
+    {
+      accessory: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: RESOURCE.ACCESSORIES,
+        required: true
+      },
+      quantity: {
+        type: Number,
+        required: true
+      }
+    }
+  ],
+  amount: {
+    type: Number,
+    required: true
+  }
+})
+
+module.exports = mongoose.model(RESOURCE.TRANSACTION, transactionSchema)
