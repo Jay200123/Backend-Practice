7ac261614c9f35cc0bf56031b2d2bfabad98482d
Jay200123 <kenken.ayonon@gmail.com>
2024-10-05 21:02:37 +0800

    Add quantity to accessory model

--- routes/accessories/model.js
+++ routes/accessories/model.js
@@ -1,22 +1,26 @@
-const mongoose = require("mongoose");
-const { RESOURCE } = require("../../constants/index");
+const mongoose = require('mongoose')
+const { RESOURCE } = require('../../constants/index')
 
 const accessoriesSchema = new mongoose.Schema({
   accessory_name: {
     type: String,
-    required: true,
+    required: true
   },
   price: {
     type: Number,
-    required: true,
+    required: true
   },
   image: [
     {
       public_id: String,
       url: String,
-      originalName: String,
-    },
+      originalName: String
+    }
   ],
-});
+  quantity: {
+    type: Number,
+    required: true
+  }
+})
 
-module.exports = mongoose.model(RESOURCE.ACCESSORIES, accessoriesSchema);
+module.exports = mongoose.model(RESOURCE.ACCESSORIES, accessoriesSchema)
