f94e78a10fade5f4066b406928715024bdb810ac
Jay200123 <kenken.ayonon@gmail.com>
2024-10-05 17:53:27 +0800

    Remove logger

--- config/db-config.js
+++ config/db-config.js
@@ -1,13 +1,11 @@
 const mongoose = require("mongoose");
 const { RESOURCE, STATUSCODE } = require("../constants/index");
-const { logger } = require("../utils/index.js");
 
 const connectDB = async () => {
   try {
     mongoose.set(RESOURCE.STRICT, false);
     await mongoose.connect(process.env.DATABASE_URI);
   } catch (err) {
-    logger.info(err);
     process.exit(STATUSCODE.ONE);
   }
 };
