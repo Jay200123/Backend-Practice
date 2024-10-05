e5cfc9d332c267674b7f8f4b0b391873fe27ffa3
Jay200123 <kenken.ayonon@gmail.com>
2024-10-05 17:53:36 +0800

    Refactor nodemailer config

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,10 +1,12 @@
 const { createTransport } = require('nodemailer');
 const { RESOURCE } = require("../constants/index");
+const globalEnviroment = require("./env-config");
+globalEnviroment();
 
 const transporter = createTransport({
     service: RESOURCE.GMAIL,   
     auth: {
-        user: process.env.USER_EMAIL,
+        user: process.env.EMAIL,
         pass: process.env.EMAIL_PASSWORD
     }
 });
