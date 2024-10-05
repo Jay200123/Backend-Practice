48afd5d3540935ae81890802a69f6df67c34113a
Jay200123 <kenken.ayonon@gmail.com>
2024-10-05 17:54:50 +0800

    Refactor sendEmail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,26 +1,28 @@
-const transporter = require(".././config/index");
-const fs = require("fs");
-const path = require("path");
-const handlebars = require("handlebars");
+const { transporter } = require('../config/index')
+const fs = require('fs')
+const path = require('path')
+const handlebars = require('handlebars')
+const juice = require("juice");
 
-const mail = path.join(__dirname, "../views/index.html");
-const content = fs.readFileSync(mail, "utf8"); 
+const mail = path.join(__dirname, '../views/index.html')
+const content = fs.readFileSync(mail, 'utf8')
+const template = handlebars.compile(content)
 
-const template = handlebars.compile(content);
 
 const sendEmail = (email, randomCode) => {
   const replacement = {
-    randomCode: randomCode,
-  };
+    randomCode: randomCode
+  }
 
-  const index = template(replacement);
+  const page = template(replacement)
+  const index = juice(page); 
 
   return transporter.sendMail({
     from: process.env.EMAIL,
     to: `${email}`,
-    subject: "Reset Account Password",
-    html: index,
-  });
-};
+    subject: 'Reset Account Password',
+    html: index
+  })
+}
 
-module.exports = sendEmail;
+module.exports = sendEmail
