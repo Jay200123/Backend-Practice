991011c736fed270c93a5758827875eccf13394e
Jay200123 <kenken.ayonon@gmail.com>
2024-10-06 10:28:46 +0800

    Add transaction routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-const { router } = require('../config/express-config');
-const { PATH } = require('../constants/index');
-const authRoutes = require('../routes/authenticate/route');
-const userRoutes = require('../routes/users/route');
-const itemRoutes = require("../routes/item/route");
-const accessoriesRoutes = require("../routes/accessories/route"); 
+const { router } = require('../config/express-config')
+const { PATH } = require('../constants/index')
+const authRoutes = require('../routes/authenticate/route')
+const userRoutes = require('../routes/users/route')
+const itemRoutes = require('../routes/item/route')
+const accessoriesRoutes = require('../routes/accessories/route')
+const transactionRoutes = require('../routes/transaction/route')
 
 const routers = [
   {
@@ -20,7 +21,11 @@ const routers = [
   },
   {
     path: PATH.ACCESSORIES,
-    route: accessoriesRoutes  
+    route: accessoriesRoutes
+  },
+  {
+    path: PATH.TRANSACTIONS,
+    route: transactionRoutes
   }
 ]
 
