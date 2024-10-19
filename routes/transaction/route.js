a7c4a0ff76cd0f0364a26be7a5bc378b57103f9d
Jay200123 <kenken.ayonon@gmail.com>
2024-10-19 09:31:21 +0800

    Refactor transaction routes

--- routes/transaction/route.js
+++ routes/transaction/route.js
@@ -1,5 +1,5 @@
 const { router } = require('../../config/index')
-const { isAuthenticated, userRole } = require("../../middleware/index")
+const { isAuthenticated, userRole } = require('../../middleware/index')
 const transactionController = require('./controller')
 const { METHOD, PATH, ROLE } = require('../../constants/index')
 
@@ -24,6 +24,13 @@ const transactionRoutes = [
     middleware: [isAuthenticated],
     role: [ROLE.ADMIN],
     handler: transactionController.createTransaction
+  },
+  {
+    method: METHOD.DELETE,
+    path: PATH.TRANSACTION_ID,
+    middleware: [isAuthenticated],
+    role: [ROLE.ADMIN],
+    handler: transactionController.deleteTransaction
   }
 ]
 
