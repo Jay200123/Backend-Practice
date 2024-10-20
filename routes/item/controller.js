39db4bd419288a61e714253046db2fda40ee69a8
Jay200123 <kenken.ayonon@gmail.com>
2024-10-20 21:02:46 +0800

    Refactor createItem

--- routes/item/controller.js
+++ routes/item/controller.js
@@ -11,14 +11,14 @@ const cloudinary = require('../../config/cloudinary')
 
 exports.getAllItems = asyncHandler(async (req, res, next) => {
   const data = await service.getAll()
-  return !data || data.length === STATUSCODE.ZERO 
+  return !data || data.length === STATUSCODE.ZERO
     ? next(new ErrorHandler('No items found'))
     : SuccessHandler(res, 'Items data successfully retrieved', data)
 })
 
 exports.getOneItem = asyncHandler(async (req, res, next) => {
   const data = await service.getById(req.params.id)
-  return !data || data.length === STATUSCODE.ZERO  
+  return !data || data.length === STATUSCODE.ZERO
     ? next(new ErrorHandler('No item found'))
     : SuccessHandler(res, 'Item found', data)
 })
@@ -26,12 +26,15 @@ exports.getOneItem = asyncHandler(async (req, res, next) => {
 exports.createItem = [
   upload.array(RESOURCE.IMAGE),
   asyncHandler(async (req, res, next) => {
-    const image = await imageUpload(req.files, []);
-    const price = Number(req.body.price);  
+    const image = await imageUpload(req.files, [])
+    const price = Number(req.body.price)
+    const parsedInfo = JSON.parse(req.body.information)
+    
     const data = await service.create({
       ...req.body,
       price: price,
       image: image,
+      information: parsedInfo
     })
 
     return SuccessHandler(res, 'Item created successfully', data)
@@ -59,7 +62,7 @@ exports.deleteOneItem = asyncHandler(async (req, res, next) => {
   const itemImage = data?.image?.map(i => i?.public_id)
   await cloudinary.api.delete_resources(itemImage)
 
-  return ! data || data.length === STATUSCODE.ZERO  
+  return !data || data.length === STATUSCODE.ZERO
     ? next(new ErrorHandler('Item not found'))
     : SuccessHandler(res, 'Item deleted successfully', data)
 })
